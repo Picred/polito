99a20f63f815d15012e07e4dcbfc1e06de8c58fd
Andrei Stefan <102211437+Picred@users.noreply.github.com>
2025-11-02 10:44:08 +0100

    Update program_1_b.s

--- ASE/lab_04/program_1_b.s
+++ ASE/lab_04/program_1_b.s
@@ -1,5 +1,5 @@
 # program_1_b.s
-# Graduate loop unrolling of program_1.s
+# Graduate loop unrolling of program_1_a.s
 
 .section .data
 i: .float 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6
